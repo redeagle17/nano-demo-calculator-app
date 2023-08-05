2981f0afc064596ac11ea950b91eb95a10a7a03b
Ankur Singh <92366204+redeagle17@users.noreply.github.com>
2023-08-06 00:02:20 +0530

    functions completed

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,59 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello, welcome to the calculator API!"};
 }
+
 crow::response add(const crow::request &req)
 {
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
+    auto json_data = crow::json::load(req.body);
+    
+    if (!json_data)
+    {
+        // Return a bad request response if the JSON data is invalid
+        return crow::response(400, "Invalid JSON data");
+    }
+    
+    if (!json_data.has("num1") || !json_data.has("num2"))
+    {
+        // Return a bad request response if the JSON data does not contain the required fields
+        return crow::response(400, "Missing 'num1' or 'num2' in JSON data");
+    }
+
+    double num1 = json_data["num1"].d();
+    double num2 = json_data["num2"].d();
+
+    double result = num1 + num2;
+
+    crow::json::wvalue response_data;
+    response_data["result"] = result;
+
+    return crow::response{response_data};
 }
+
 crow::response subtract(const crow::request &req)
 {
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
+    auto json_data = crow::json::load(req.body);
+    
+    if (!json_data)
+    {
+        // Return a bad request response if the JSON data is invalid
+        return crow::response(400, "Invalid JSON data");
+    }
+    
+    if (!json_data.has("num1") || !json_data.has("num2"))
+    {
+        // Return a bad request response if the JSON data does not contain the required fields
+        return crow::response(400, "Missing 'num1' or 'num2' in JSON data");
+    }
+
+    double num1 = json_data["num1"].d();
+    double num2 = json_data["num2"].d();
+
+    double result = num1 - num2;
+
+    crow::json::wvalue response_data;
+    response_data["result"] = result;
+
+    return crow::response{response_data};
 }
